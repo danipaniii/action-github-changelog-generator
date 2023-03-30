161eaa5f82bb4171b259a6ac5030658d505623df
Daniel Priebe <danielpp1450@gmail.com>
2023-03-30 13:41:41 +0200

    test

--- entrypoint.sh
+++ entrypoint.sh
@@ -161,7 +161,7 @@ fi
 if [[ -e "$FILE" ]]; then
   CONTENT=$(cat "$FILE")
   echo "CONTENT: ${CONTENT}" 
-  echo "" > CHANGELOG_FINAL
+  echo "" > $CHANGELOG_FINAL
   while IFS= read -r line ; do echo $line >> CHANGELOG_FINAL; done <<< "$CONTENT"
   echo "FINAL_CHANGELOG: ${CHANGELOG_FINAL}"
   echo "changelog=$CHANGELOG_FINAL" >> $GITHUB_OUTPUT
